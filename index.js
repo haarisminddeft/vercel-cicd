7643426b4e13bce16fb6c9bf0a4709d3c371bae7
haarisminddeft <haris.khatri@minddeft.net>
2024-02-28 13:07:26 +0530

    Attack App to Test

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-// Import the express library
-const express = require('express');
+const attack = require('./attack');
 
-// Create an instance of express
-const app = express();
-
-// Define a route handler for GET requests made to the root path ('/')
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-// Have the app listen on port 3000
-app.listen(3000, () => {
-  console.log('App is listening on port 3000');
-});
+console.log(
+  attack('Smelly Ogre', 23),
+);
